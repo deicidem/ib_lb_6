5ee7fc65ec8def2fbfc68642ecfcd6b7c27a775c
Andrey <kronosdidi@gmail.com>
2021-04-22 13:28:17 +0300

    add encryption

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,25 +11,36 @@ public class Main {
         int sizeX = 6;
         int sizeY = 4;
         String message = "Привет, сегодня 22 апреля 2021 года. Четверг";
-        String order = "462531";
-        encode(message, sizeX, sizeY, order);
+        String order = "351420";
+        System.out.println(encode(message, sizeX, sizeY, order));
     }
     public static String encode(String message,int sizeX,int sizeY,String order){
-        String result = "";
+        StringBuilder result = new StringBuilder();
         List<String[][]> tables = new ArrayList<>();
-        int index; //Номер таблицы
         for (int i = 0; i < message.length(); i++) {
-            index = i / (sizeX * sizeY);
+            int index = i / (sizeX * sizeY); // Номер таблицы
             if (tables.size() <= index ) {
                 tables.add(new String[sizeY][sizeX]);
             }
             tables.get(index)[i/sizeX % sizeY][i%sizeX] = String.valueOf(message.charAt(i));
         }
+
+        for (String[][] table: tables) {
+            for (int i = 0; i < order.length(); i++) {
+                int index = order.indexOf(String.valueOf(i)); // индекс номера столбца в order
+                for (String[] strings : table) {
+                    if (strings[index] != null) {
+                        result.append(strings[index]);
+                    }
+                }
+            }
+        }
+
         for (String[][] table : tables) {
             for (String[] strings : table) {
                 System.out.println(Arrays.toString(strings));
             }
         }
-        return result;
+        return result.toString();
     }
 }
